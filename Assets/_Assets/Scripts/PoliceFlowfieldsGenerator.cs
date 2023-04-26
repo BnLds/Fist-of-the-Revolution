5af59c9d09c85acc3d1d7dee9275a196d58518fd
BnLds <landes.benjamin@gmail.com>
2023-04-26 18:30:55 +0200

    removed singleton pattern which was useless

--- Assets/_Assets/Scripts/PoliceFlowfieldsGenerator.cs
+++ Assets/_Assets/Scripts/PoliceFlowfieldsGenerator.cs
@@ -1,24 +1,7 @@
 using UnityEngine;
-using System.Collections.Generic;
-using UnityEngine.Events;
 
 public class PoliceFlowfieldsGenerator : MonoBehaviour
 {
-    //public static PoliceFlowfieldsGenerator Instance { get; private set; }
-
-    private void Awake()
-    {
-        /*if(Instance != null)
-        {
-            Destroy(this);
-        }
-        else
-        {
-            Instance = this;
-        }
-        */
-    }
-
     public FlowField CreateNewFlowField(Transform target)
     {
         return GridController.Instance.GenerateFlowField(target);

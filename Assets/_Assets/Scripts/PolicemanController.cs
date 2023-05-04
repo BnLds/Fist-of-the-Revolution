be9c0face16a7e29dc42d4924ed2efa197abb91f
BnLds <landes.benjamin@gmail.com>
2023-05-04 11:54:45 +0200

    Used the Protester AI and Protester Data to make the Pooliceman follow the protest when in this state

--- Assets/_Assets/Scripts/PolicemanController.cs
+++ Assets/_Assets/Scripts/PolicemanController.cs
@@ -4,27 +4,41 @@ public class PolicemanController : MonoBehaviour
 {
     //[SerializeField] private PolicemanAI _policemanAI;
     [SerializeField] private PoliceUnitSM _policeUnitSM;
+    [SerializeField] private ProtesterAI _protesterAI;
     [SerializeField] private float _moveSpeed = 3f;
     
-    private Vector3 _moveDirection;
+    private Vector3 _moveDirectionFollowProtest;
     private Rigidbody _policemanRB;
 
     private void Awake()
     {
-        _moveDirection = Vector3.zero;
+        _moveDirectionFollowProtest = Vector3.zero;
         _policemanRB = GetComponent<Rigidbody>();
     }
-    /*private void Start()
+    private void Start()
     {
-        _policemanAI.OnMoveDirectionInput.AddListener(policemanAI_OnMoveDirectionInput);
+        //_policemanAI.OnMoveDirectionInput.AddListener(policemanAI_OnMoveDirectionInput);
+        _protesterAI.OnMoveDirectionInput.AddListener(protesterAI_OnMoveDirectionInput);
     }
-    */
+    
 
     private void FixedUpdate()
     {
-        //_policemanRB.velocity = _moveDirection * _moveSpeed;
-        _policemanRB.velocity =  _policeUnitSM.MoveDirectionInput * _moveSpeed;
+        if(_policeUnitSM.CurrentState == _policeUnitSM.FollowProtestState && _policeUnitSM.FollowProtestState.IsFollowingProtest)
+        {
+            //follow the protest path
+            _policemanRB.velocity = _moveDirectionFollowProtest * _moveSpeed; 
+        }
+        else
+        {
 
+            _policemanRB.velocity =  _policeUnitSM.MoveDirectionInput * _moveSpeed;
+        }
+    }
+
+    private void protesterAI_OnMoveDirectionInput(Vector3 direction)
+    {
+        _moveDirectionFollowProtest = direction;
     }
 
     /*private void policemanAI_OnMoveDirectionInput(Vector3 direction)

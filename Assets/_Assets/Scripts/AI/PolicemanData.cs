4dbfe1658fafcffb15f2eaf39125e45589e92f51
BnLds <landes.benjamin@gmail.com>
2023-05-04 11:18:36 +0200

    Store the currently watched object

--- Assets/_Assets/Scripts/AI/PolicemanData.cs
+++ Assets/_Assets/Scripts/AI/PolicemanData.cs
@@ -6,4 +6,5 @@ public class PolicemanData : AIData
     public List<Transform> WatchedObjectsInReactionRange;
     public FlowField CurrentFlowField = null;
     public Vector3 CurrentWatchObjectPosition;
+    public Transform CurrentWatchedObject;
 }

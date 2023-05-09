60bca9481677ca5181e7c166f906ef13278c1553
BnLds <landes.benjamin@gmail.com>
2023-05-09 12:33:07 +0200

    modified target detection to be based on tracked suspects list and still go through checks to ensure the targets are in line of sight

--- Assets/_Assets/Scripts/AI/TargetDetector.cs
+++ Assets/_Assets/Scripts/AI/TargetDetector.cs
@@ -5,48 +5,48 @@ using UnityEngine;
 public class TargetDetector : Detector
 {
     [SerializeField] private float _targetDetectionRange = 20f;
-    [SerializeField] private LayerMask _obstaclesLayerMask, _targetsLayerMask;
+    [SerializeField] private LayerMask _obstaclesLayerMask;
+    [SerializeField] private LayerMask[] _targetsLayerMask;
     [SerializeField] private bool _showGizmos = false;
 
     private List<Transform> _colliders;
+    private Vector3 _directionGizmo;
 
     public override void Detect(AIData aiData)
     {
         Collider[] targetColliders;
         if(aiData is PolicemanData policemandData)
         {
-            //targetColliders = PoliceResponseData.TrackedSuspects.Select(_ => _.SuspectTransform.GetComponent<Collider>()).ToArray();
-            aiData.Targets = PoliceResponseData.TrackedSuspects.Select(_ => _.SuspectTransform).ToList();
+            targetColliders = PoliceResponseData.TrackedSuspects.Select(_ => _.SuspectTransform.GetComponent<Collider>()).ToArray();
         }
         else
         {
+            //unused at the moment but run
             //find out if targets are near
-            targetColliders = Physics.OverlapSphere(transform.position, _targetDetectionRange, _targetsLayerMask);
-            if (targetColliders.Length != 0)
-            {
-                _colliders = new List<Transform>();
-                foreach (Collider target in targetColliders)
-                {
-                    //check if it can see the first target
-                    Vector3 direction = (target.transform.position - transform.position).normalized;
-
-                    //targets are also on the obstaclesLayerMask
-                    bool canSeePlayer = Physics.Raycast(transform.position, direction, out RaycastHit hitInfo, _targetDetectionRange, _obstaclesLayerMask);
-
-                    //Make sure the collider we see is on the targetLayer.
-                    if (canSeePlayer && 1 << hitInfo.collider.gameObject.layer == _targetsLayerMask.value)
-                    {
-
-                        //Debug.DrawRay(transform.position, direction * targetDetectionRange, Color.blue);
-                        _colliders.Add(target.transform);
-                    }
-                }
-            }
-            aiData.Targets = _colliders;
+            //targetColliders = Physics.OverlapSphere(transform.position, _targetDetectionRange, _targetsLayerMask);
+            targetColliders = null;
         }
 
+        if (targetColliders!=null && targetColliders.Length != 0)
+        {
+            _colliders = new List<Transform>();
+            foreach (Collider target in targetColliders)
+            {
+                //check if it can see the first target
+                Vector3 direction = (target.transform.position - transform.position).normalized;
 
-        
+                //targets are also on the obstaclesLayerMask
+                bool canSeePlayer = Physics.Raycast(transform.position, direction, out RaycastHit hitInfo, _targetDetectionRange, _obstaclesLayerMask);
+
+                //Make sure the collider we see is on the targetLayer.
+                if (canSeePlayer && _targetsLayerMask.FirstOrDefault(_ => _.value == 1 << hitInfo.collider.gameObject.layer) != 0)
+                {
+                    //Debug.DrawRay(transform.position, direction * targetDetectionRange, Color.blue);
+                    _colliders.Add(target.transform);
+                }
+            }
+        }
+        aiData.Targets = _colliders;
     }
 
     private void OnDrawGizmos()
@@ -59,7 +59,7 @@ public class TargetDetector : Detector
 
         foreach(Transform collider in _colliders)
         {
-            Gizmos.DrawIcon(collider.position + Vector3.up*2, "32");
+            Gizmos.DrawIcon(collider.position + Vector3.up*2, "32"); 
         }
     }
 }

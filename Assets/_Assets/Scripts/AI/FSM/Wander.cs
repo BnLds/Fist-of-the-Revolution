d6e830e050aa88e7eeb6dee638e42475695e5944
BnLds <landes.benjamin@gmail.com>
2023-05-10 11:00:34 +0200

    solved bug with creation of wander points and refined it

--- Assets/_Assets/Scripts/AI/FSM/Wander.cs
+++ Assets/_Assets/Scripts/AI/FSM/Wander.cs
@@ -5,8 +5,11 @@ public class Wander : BaseState
 {
     private PoliceUnitSM _policeUnitSM;
     private Vector3 _wanderPoint;
+    private bool _isFirstWanderPoint;
     private float _detectionDelay;
     private float _wanderTime;
+    private float _wanderRandomDistanceMax;
+    private float _wanderPointReachedRange = 2f;
 
 
     public Wander(PoliceUnitSM stateMachine) : base("Wander", stateMachine)
@@ -19,11 +22,11 @@ public class Wander : BaseState
         base.Enter();
 
         _wanderTime = _policeUnitSM.WanderDuration;
+        _wanderRandomDistanceMax = _policeUnitSM.WanderRandomDistanceMax;
+        _isFirstWanderPoint = true;
 
-        Vector3 _wanderPoint = new Vector3(PlayerController.Instance.transform.position.x + Random.Range(0f, 10f), PlayerController.Instance.transform.position.y, PlayerController.Instance.transform.position.z + Random.Range(0f, 10f));
 
-        //Generate flowfield to next wander point
-        _policeUnitSM.PoliceUnitData.CurrentFlowField = GridController.Instance.GenerateFlowField(_wanderPoint);
+        CreateNewWanderPoint();
     }
 
     public override void UpdateLogic()
@@ -57,14 +60,11 @@ public class Wander : BaseState
     {
         base.UpdatePhysics();
 
-        float wanderPointReachedRange = 2f;
         //check if the policeman is within distance of the wander point
-        if (Utility.Distance2DBetweenVector3(_wanderPoint, _policeUnitSM.transform.position) < wanderPointReachedRange)
+        if (Utility.Distance2DBetweenVector3(_wanderPoint, _policeUnitSM.transform.position) < _wanderPointReachedRange)
         {
-            //stop and reset flowfield
-            Vector3 _wanderPoint = new Vector3(PlayerController.Instance.transform.position.x + Random.Range(0f, 10f), PlayerController.Instance.transform.position.y, PlayerController.Instance.transform.position.z + Random.Range(0f, 10f));
-            //Generate flowfield to next wander point
-            _policeUnitSM.PoliceUnitData.CurrentFlowField = GridController.Instance.GenerateFlowField(_wanderPoint);
+            Debug.Log("WanderPoint reached");
+            CreateNewWanderPoint();
         }
         else if (_policeUnitSM.PoliceUnitData.CurrentFlowField != null)
         {
@@ -77,6 +77,24 @@ public class Wander : BaseState
     {
         base.Exit();
         _policeUnitSM.IsTargetLost = false;
+        Debug.Log("Exiting Wander state");
+    }
+
+    private void CreateNewWanderPoint()
+    {
+        if(!_isFirstWanderPoint)
+        {
+            _wanderPoint = new Vector3(_wanderPoint.x + Random.Range(0f, _wanderRandomDistanceMax), _wanderPoint.y, _wanderPoint.z + Random.Range(0f, _wanderRandomDistanceMax));
+        }
+        else
+        {
+            _wanderPoint = new Vector3(PlayerController.Instance.transform.position.x + Random.Range(0f, _wanderRandomDistanceMax), PlayerController.Instance.transform.position.y, PlayerController.Instance.transform.position.z + Random.Range(0f, _wanderRandomDistanceMax));
+        }
+
+        _isFirstWanderPoint = false;
+
+        //Generate flowfield to next wander point
+        _policeUnitSM.PoliceUnitData.CurrentFlowField = GridController.Instance.GenerateFlowField(_wanderPoint);
     }
 
 }

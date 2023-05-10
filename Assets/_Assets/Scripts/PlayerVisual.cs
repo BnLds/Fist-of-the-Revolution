192c7fd9d9b3fc68ce6981ff9458c6a751705ca2
BnLds <landes.benjamin@gmail.com>
2023-05-10 11:42:04 +0200

    player visual script and object created

---
+++ Assets/_Assets/Scripts/PlayerVisual.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class PlayerVisual : MonoBehaviour
+{
+    [SerializeField] private List<SkinSO> _skinSOs;
+
+    private MeshRenderer _meshRenderer;
+    private SkinSO _currentSkin;
+
+    private void Awake()
+    {
+        _meshRenderer = GetComponent<MeshRenderer>();
+        _currentSkin = _skinSOs[0];
+        _meshRenderer.material = _currentSkin.skinMaterial;
+    }
+
+    public SkinSO GetSkinSO()
+    {
+        return _currentSkin;
+    }
+}

a58530feda2b06a32b57634fbd416e3ecc9a54eb
BnLds <landes.benjamin@gmail.com>
2023-05-19 16:05:46 +0200

    Added different character visuals for protesters

--- Assets/_Assets/Scripts/ProtesterVisual.cs
+++ Assets/_Assets/Scripts/ProtesterVisual.cs
@@ -5,21 +5,49 @@ public class ProtesterVisual : MonoBehaviour
 {
     [Header("Initialization Parameters")]
     [SerializeField] private ProtesterData _protesterData;
+    [SerializeField] private ProtesterController _protesterController;
     [SerializeField] private LayerMask _floorMask;
     [SerializeField] private List<SkinSO> _skinSOs;
+    [SerializeField] private List<MeshRenderer> _coloredClothes;
+    [SerializeField] private List<Transform> _visualComponents;
+    [SerializeField] private List<Transform> _attributes;
 
-    private MeshRenderer _meshRenderer;
-
-    private void Awake()
-    {
-        _meshRenderer = GetComponent<MeshRenderer>();
-    }
+    private Vector3 _targetDirection;
 
     private void Start()
     {
+        _protesterController.OnMove.AddListener(ProtesterController_OnMove);
+
         int randomIndex = Random.Range(0, _skinSOs.Count);
-        _meshRenderer.material = _skinSOs[randomIndex].skinMaterial;
+
+        foreach(MeshRenderer meshRenderer in _coloredClothes)
+        {
+            meshRenderer.material = _skinSOs[randomIndex].skinMaterial;
+        }
         _protesterData.Skin = _skinSOs[randomIndex];
+
+        foreach(Transform attribute in _attributes)
+        {
+            attribute.gameObject.SetActive(false);
+        }
+        //draw a random value to see if protester should wear an attribute or no
+        int minRange = 1;
+        int maxRange = 100;
+        int draw = Random.Range(minRange, maxRange);
+        int meanValue = Mathf.CeilToInt(((float)maxRange - (float)minRange) / (float)2);
+        if(draw > meanValue)
+        {
+            for (int i = 0; i < _attributes.Count; i++)
+            {
+                int lowerBound = Mathf.CeilToInt(meanValue * (1 + (float)i / (float)_attributes.Count));
+                int higherBound = Mathf.CeilToInt(meanValue * (1 + (float)(1 + i) / (float)_attributes.Count));
+                if(draw> lowerBound && draw <= higherBound)
+                {
+                    _attributes[i].gameObject.SetActive(true);
+                    break;
+                }
+            }
+        }
     }
 
     private void Update()
@@ -33,16 +61,33 @@ public class ProtesterVisual : MonoBehaviour
         {
             Show();
         }
+
+        Quaternion targetRotation = Quaternion.LookRotation(_targetDirection);
+        float turnSpeed = 4f;
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * turnSpeed);
     }
     
+    private void ProtesterController_OnMove(Vector3 direction)
+    {
+        if(direction != Vector3.zero)
+        {
+            _targetDirection = -direction;
+        }
+    }
 
     public void Show()
     {
-        _meshRenderer.enabled = true;
+        foreach(Transform component in _visualComponents)
+        {
+            component.gameObject.SetActive(true);
+        }
     }
 
     public void Hide()
     {
-        _meshRenderer.enabled = false;
+        foreach(Transform component in _visualComponents)
+        {
+            component.gameObject.SetActive(false);
+        }
     }
 }

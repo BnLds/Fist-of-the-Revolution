fe23012bbc03a62fe1e33aed863234f4f51921d7
BnLds <landes.benjamin@gmail.com>
2023-05-22 17:15:00 +0200

    created main menu and scene loader

---
+++ Assets/_Assets/Scripts/UI/MainMenuUI.cs
@@ -0,0 +1,31 @@
+using UnityEngine.UI;
+using UnityEngine;
+
+public class MainMenuUI : MonoBehaviour
+{
+    [SerializeField] private Button playButton;
+    [SerializeField] private Button quitButton;
+
+
+
+
+    private void Awake()
+    {
+        playButton.onClick.AddListener(() => 
+        {
+            //Click
+            Loader.Load(Loader.Scene.GameScene);
+        });
+
+        quitButton.onClick.AddListener(() => 
+        {
+            //Quit
+            Application.Quit();
+
+        });
+
+        Time.timeScale = 1f;
+        
+    }
+
+}

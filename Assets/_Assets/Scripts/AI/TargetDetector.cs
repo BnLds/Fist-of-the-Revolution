a9649279d18db45212cddb18120edbc4f8ebceca
Benjamin Landes <90406524+BnLds@users.noreply.github.com>
2023-06-20 15:48:09 +0200

    Update TargetDetector.cs
    optimized code to remove GC allocation

--- Assets/_Assets/Scripts/AI/TargetDetector.cs
+++ Assets/_Assets/Scripts/AI/TargetDetector.cs
@@ -7,47 +7,40 @@ public class TargetDetector : Detector
     [SerializeField] private float _targetDetectionRange = 20f;
     [SerializeField] private LayerMask _obstaclesLayerMask;
     [SerializeField] private LayerMask[] _targetsLayerMask;
-    [SerializeField] private bool _showGizmos = false;
+    //[SerializeField] private bool _showGizmos = false;
 
-    private List<Transform> _colliders;
     private Vector3 _directionGizmo;
 
     public override void Detect(AIData aiData)
     {
-        Collider[] targetColliders;
         if(aiData is PolicemanData policemandData)
         {
-            targetColliders = PoliceResponseManager.Instance.GetTrackedList().Select(_ => _.SuspectTransform.GetComponent<Collider>()).ToArray();
-        }
-        else
-        {
-            //unused at the moment but run
-            //find out if targets are near
-            targetColliders = Physics.OverlapSphere(transform.position, _targetDetectionRange, _targetsLayerMask.Sum(_=>_.value));
-        }
+            aiData.Targets.Clear();
 
-        if (targetColliders!=null && targetColliders.Length != 0)
-        {
-            _colliders = new List<Transform>();
-            foreach (Collider target in targetColliders)
+            var targetColliders = PoliceResponseManager.Instance.GetTrackedList().Select(_ => _.SuspectTransform.GetComponent<Collider>());
+            
+            if (targetColliders!=null && targetColliders.Count() != 0)
             {
-                //check if it can see the first target
-                Vector3 direction = (target.transform.position - transform.position).normalized;
-
-                //targets are also on the obstaclesLayerMask
-                bool isPlayerInDetectionRange = Physics.Raycast(transform.position, direction, out RaycastHit hitInfo, _targetDetectionRange, _obstaclesLayerMask);
-
-                //Make sure the collider we see is on the targetLayer.
-                if (isPlayerInDetectionRange && _targetsLayerMask.FirstOrDefault(_ => _.value == 1 << hitInfo.collider.gameObject.layer) != 0)
+                foreach (Collider target in targetColliders)
                 {
-                    //Debug.DrawRay(transform.position, direction * targetDetectionRange, Color.blue);
-                    _colliders.Add(target.transform);
+                    //check if it can see the first target
+                    Vector3 direction = (target.transform.position - transform.position).normalized;
+
+                    //targets are also on the obstaclesLayerMask
+                    bool isPlayerInDetectionRange = Physics.Raycast(transform.position, direction, out RaycastHit hitInfo, _targetDetectionRange, _obstaclesLayerMask);
+
+                    //Make sure the collider we see is on the targetLayer.
+                    if (isPlayerInDetectionRange && _targetsLayerMask.FirstOrDefault(_ => _.value == 1 << hitInfo.collider.gameObject.layer) != 0)
+                    {
+                        //Debug.DrawRay(transform.position, direction * targetDetectionRange, Color.blue);
+                        aiData.Targets.Add(target.transform);
+                    }
                 }
             }
         }
-        aiData.Targets = _colliders;
     }
 
+/*
     private void OnDrawGizmos()
     {
         if(!_showGizmos) return;
@@ -61,4 +54,5 @@ public class TargetDetector : Detector
             Gizmos.DrawIcon(collider.position + Vector3.up*2, "32"); 
         }
     }
+*/
 }

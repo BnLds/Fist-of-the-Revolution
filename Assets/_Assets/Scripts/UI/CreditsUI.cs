676b6e3ee363cf20d3cc3fdd0198ad1fdfd82304
Benjamin Landes <90406524+BnLds@users.noreply.github.com>
2023-07-20 15:57:33 +0200

    created credits UI

---
+++ Assets/_Assets/Scripts/UI/CreditsUI.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CreditsUI : MonoBehaviour
+{
+    [SerializeField] private Button _closeButton;
+    [SerializeField] private TextMeshProUGUI _closeText;
+    [SerializeField] private TextMeshProUGUI _titleText;
+    [SerializeField] private TextMeshProUGUI _musicText;
+    [SerializeField] private TextMeshProUGUI _cityText;
+    [SerializeField] private TextMeshProUGUI _charactersText;
+    [SerializeField] private TextMeshProUGUI _soundsText;
+
+    private void Awake()
+    {
+        _closeButton.onClick.AddListener(() =>
+        {
+            Hide();
+        });
+    }
+
+    private void Start()
+    {
+        _GameManager.Instance.OnGameUnpaused.AddListener(GameManager_OnGameUnpaused);
+        Hide();
+    }
+
+    private void GameManager_OnGameUnpaused()
+    {
+        Hide();
+    }
+
+    public void Show()
+    {
+        LocalizeStrings();
+        gameObject.SetActive(true);
+    }
+
+    private void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+
+    private void LocalizeStrings()
+    {
+        _closeText.text = Localizer.Instance.GetMessage(LocalizationKeys.CLOSE_KEY);
+        _titleText.text = Localizer.Instance.GetMessage(LocalizationKeys.CREDITS_KEY);
+        _musicText.text = Localizer.Instance.GetMessage(LocalizationKeys.MUSIC_KEY) + ": Kyubz";
+        _cityText.text = Localizer.Instance.GetMessage(LocalizationKeys.CITY_KEY) + ": Kenney";
+        _charactersText.text = Localizer.Instance.GetMessage(LocalizationKeys.CHARACTERS_KEY) + ": Bit Gamey";
+        _soundsText.text = Localizer.Instance.GetMessage(LocalizationKeys.SOUND_EFFECTS_KEY) + ": 3maze/Gamemaster Audio/Glitchedtones/West Wolf";
+    }
+    
+}
--- Assets/_Assets/Scripts/UI/MainMenuUI.cs
+++ Assets/_Assets/Scripts/UI/MainMenuUI.cs
@@ -1,12 +1,19 @@
 using UnityEngine.UI;
 using UnityEngine;
+using TMPro;
 
 public class MainMenuUI : MonoBehaviour
 {
     [SerializeField] private Button _playButton;
+    [SerializeField] private TextMeshProUGUI _playText;
     [SerializeField] private Button _quitButton;
+    [SerializeField] private TextMeshProUGUI _quitText;
     [SerializeField] private Button _optionsButton;
+    [SerializeField] private TextMeshProUGUI _optionsText;
+    [SerializeField] private Button _creditsButton;
+    [SerializeField] private TextMeshProUGUI _creditsText;
     [SerializeField] private OptionsUI _optionsUI;
+    [SerializeField] private CreditsUI _creditsUI;
 
     private void Awake()
     {
@@ -29,6 +36,37 @@ public class MainMenuUI : MonoBehaviour
             _optionsUI.Show();
         });
 
+        _creditsButton.onClick.AddListener(() =>
+        {
+            SoundManager.Instance.PlayButtonClickSound();
+            _creditsUI.Show();
+        });
+
         Time.timeScale = 1f;
+    }
+
+    private void Start()
+    {
+        Localizer.Instance.LocalizationLoaded.AddListener(() => {
+            LocalizeStrings();
+        });
+
+        _optionsUI.OnReturnToMainMenu.AddListener(() =>
+        {
+            LocalizeStrings();
+        });
+    }
+
+    private void OnDisable()
+    {
+        _optionsUI.OnReturnToMainMenu.RemoveAllListeners();
+    }
+
+    private void LocalizeStrings()
+    {
+        _optionsText.text = Localizer.Instance.GetMessage(LocalizationKeys.OPTIONS_KEY);
+        _quitText.text = Localizer.Instance.GetMessage(LocalizationKeys.QUIT_KEY);
+        _playText.text = Localizer.Instance.GetMessage(LocalizationKeys.PLAY_KEY);
+        _creditsText.text = Localizer.Instance.GetMessage(LocalizationKeys.CREDITS_KEY);
     }
 }
